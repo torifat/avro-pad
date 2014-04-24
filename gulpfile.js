ade7e053a8c613e2ffcbd5a547bf75025ab3cc82
Rifat Nabi <to.rifat@gmail.com>
2014-04-24 21:33:44 +0600

    updated design

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp'),
     lr = require('gulp-livereload'),
-    dest = 'src';
+    dest = 'src',
+    bower = 'bower_components';
 
 gulp.task('server', function(next) {
   var connect = require('connect'),
       server = connect();
-  server.use(connect.static(dest)).listen(process.env.PORT || 8080, next);
+  server
+    .use(connect.static(dest))
+    .use(connect.static(bower))
+    .listen(process.env.PORT || 8080, next);
 });
 
 gulp.task('watch', ['server'], function() {
