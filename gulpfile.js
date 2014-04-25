977f9e9ea0e4d72af2c4b6c32a2689f438ea6e09
Rifat Nabi <to.rifat@gmail.com>
2014-04-25 23:46:06 +0600

    added tooltip, updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
 var gulp = require('gulp'),
     lr = require('gulp-livereload'),
+    opn = require('opn'),
+    chalk = require('chalk'),
     dest = 'src',
     bower = 'bower_components';
 
 gulp.task('server', function(next) {
   var connect = require('connect'),
-      server = connect();
+      server = connect(),
+      host = 'localhost',
+      port = process.env.PORT || 8080;
+
   server
     .use(connect.static(dest))
     .use(connect.static(bower))
-    .listen(process.env.PORT || 8080, next);
+    .listen(port, next)
+    .on('listening', function () {
+        console.log(chalk.green('Started dev server on http://' + host + ':' + port));
+        opn('http://' + host + ':' + port);
+    });
 });
 
 gulp.task('watch', ['server'], function() {
