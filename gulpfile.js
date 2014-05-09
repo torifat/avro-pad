eaf68c87ac0861480300ece9090a1c8ca1f37adc
Rifat Nabi <to.rifat@gmail.com>
2014-05-10 02:17:10 +0600

    :muscle: improved the build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 var gulp = require('gulp'),
-    lr = require('gulp-livereload'),
+    rev = require('gulp-rev'),
     clean = require('gulp-clean'),
+    lr = require('gulp-livereload'),
     useref = require('gulp-useref'),
     filter = require('gulp-filter'),
     uglify = require('gulp-uglify'),
+    manifest = require('gulp-manifest'),
     filesize = require('gulp-filesize'),
     minifyCss = require('gulp-minify-css'),
+    revReplace = require('gulp-rev-replace'),
     opn = require('opn'),
     chalk = require('chalk'),
     connect = require('connect'),
     src = 'src',
-    dist = 'dist',
+    build = 'build',
     host = 'localhost';
 
 gulp.task('server', function(next) {
@@ -27,19 +30,28 @@ gulp.task('server', function(next) {
 });
 
 gulp.task('clean', function () {
-  return gulp.src(dist, {read: false})
+  return gulp.src(build, {read: false})
     .pipe(clean());
 });
 
-gulp.task('assets', ['clean'], function () {
-  return gulp.src([
-      src + '/images/**',
-      src + '/app.appcache'
-    ], {base: src})
-    .pipe(gulp.dest(dist));
+gulp.task('images', ['clean'], function () {
+  return gulp.src(src + '/images/**', {base: src})
+    .pipe(gulp.dest(build));
 });
 
-gulp.task('build', ['clean', 'assets'], function () {
+gulp.task('manifest', ['assets'], function(){
+  gulp.src([build + '/**'])
+    .pipe(manifest({
+      hash: true,
+      preferOnline: true,
+      network: ['http://*', 'https://*', '*'],
+      filename: 'app.manifest',
+      exclude: 'app.manifest'
+     }))
+    .pipe(gulp.dest(build));
+});
+
+gulp.task('assets', ['clean', 'images'], function () {
   var jsFilter = filter('**/*.js'),
       cssFilter = filter('**/*.css');
 
@@ -53,9 +65,11 @@ gulp.task('build', ['clean', 'assets'], function () {
     .pipe(minifyCss())
     .pipe(filesize())
     .pipe(cssFilter.restore())
+    .pipe(rev())
     .pipe(useref.restore())
     .pipe(useref())
-    .pipe(gulp.dest(dist));
+    .pipe(revReplace())
+    .pipe(gulp.dest(build));
 });
 
 gulp.task('watch', ['server'], function() {
@@ -65,12 +79,14 @@ gulp.task('watch', ['server'], function() {
   });
 });
 
+gulp.task('build', ['images', 'assets', 'manifest']);
+
 gulp.task('default', ['build'], function (next) {
   var server = connect(),
       port = process.env.PORT || 8888;
 
   server
-    .use(connect.static(dist))
+    .use(connect.static(build))
     .listen(port, next)
     .on('listening', function () {
         console.log(chalk.green('Started server on http://' + host + ':' + port));
